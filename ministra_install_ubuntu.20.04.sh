e5f121e4a2a29e024e5b2c07c0746834b7d050c8
madzharov <rosen8012@abv.bg>
2024-09-01 10:49:36 +0200

    Update ministra_install_ubuntu.20.04.sh

--- ministra_install_ubuntu.20.04.sh
+++ ministra_install_ubuntu.20.04.sh
@@ -9,7 +9,7 @@ apt-get install net-tools -y
 VERSION="5.6.8"
 TIME_ZONE="Europe/Amsterdam" #
 mysql_root_password="test123456"
-repository="https://manner-rising-st-voting.trycloudflare.com/stalker"
+repository="https://headlines-respectively-definitely-gem.trycloudflare.com/stalker"
 
 # SET LOCALE TO UTF-8
 function setLocale {
